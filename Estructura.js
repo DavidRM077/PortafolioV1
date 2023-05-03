67cf46987f5209a6494a9efb8fbdacf9529dbcce
DavidR <davidrap087@gmail.com>
2023-05-03 19:36:27 -0400

    Primer commit de las modificaciones

---
+++ Estructura.js
@@ -0,0 +1,26 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+window.addEventListener("load", () => {
+  const currentDate = new Date();
+  currentYear.innerText = currentDate.getFullYear();
+});
+
+
+var botonAudio = document.getElementById("escucha")
+var parrafo = document.getElementById("parrafoSobremi")
+
+function hablar () 
+{
+    let texto = parrafo.textContent
+    let voz = new SpeechSynthesisUtterance() //crear nueva voz
+    voz.text = texto
+    voz.lang = "es-Es"
+    window.speechSynthesis.speak(voz)
+}
+
+botonAudio.onclick = hablar()
